155b85486012861c6de0a02031d9d655120f62c8
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-01-17 16:24:04 +0530

    Filtering Search

--- djfilter/core/views.py
+++ djfilter/core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from .models import Journal
 def BootstrapFilterView(request):
+    qs = Journal.objects.all()
+    title_contains_query = request.GET.get('tittle_contains')
+    title_exact_query = request.GET.get('tittle_exact')
+    title_or_author_query = request.GET.get('tittle_or_author')
+    context = {
+        'queryset': qs,
+
+    }
     return render(request, "bootstrap_form.html" ,{})
 
