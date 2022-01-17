0693384485312c7797b8cc0e683622a5ddbcf683
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-01-17 12:28:52 +0530

    Form Content

--- djfilter/djfilter/urls.py
+++ djfilter/djfilter/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import path
+from core.views import BootstrapFilterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',BootstrapFilterView, name = 'bootstrap')
 ]
 
 if settings.DEBUG:
